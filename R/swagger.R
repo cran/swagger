94321ae8476eb3eb8bfae1099fafb8ea49f7d30c
Javier Luraschi <javier@rstudio.com>
2017-10-27 07:09:34 +0000

    version 3.4.0

---
+++ R/swagger.R
@@ -0,0 +1,12 @@
+#' swagger: Dynamically Generates Documentation from a Swagger-Compliant API
+#'
+#' Swagger is a collection of HTML, Javascript, and CSS assets that
+#' dynamically generate beautiful documentation from a Swagger-compliant API.
+#'
+#' The main purpose of this package is to enable package authors to create
+#' APIs that are compatible with \url{https://swagger.io/}
+#' and \url{https://www.openapis.org/}.
+#'
+#' To learn more about Swagger visit: \url{https://swagger.io/swagger-ui/}
+#'
+"_PACKAGE"
